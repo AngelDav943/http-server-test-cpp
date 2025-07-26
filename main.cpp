f534ea8d5bf45445593fc2f0948da9330ef04a8b
Angel <angel@angeldav.net>
2025-07-26 14:50:19 -0500

    some more changes

--- main.cpp
+++ main.cpp
@@ -7,19 +7,19 @@ int main() {
 
 	int number = 0;
 
-	server.Get("/", [&](const Request&, Response& res) {
-		res.set_content("Hello, world!", "text/plain");
-	});
-
 	server.Get("/count", [&](const Request&, Response& res) {
 		number++;
-		res.set_content("Counted!", "text/plain");
+		printf("Counted!\n");
+		res.set_content(to_string(number), "text/plain");
 	});
 
 	server.Get("/get_count", [&](const Request&, Response& res) {
 		// res.set_content("Hello, world!", "text/plain");
+		printf("%s\n", ("counter: " + to_string(number)).c_str());
 		res.set_content("counter: " + to_string(number), "text/plain");
 	});
+
+	server.set_mount_point("/", "./public");
 
 	server.listen("0.0.0.0", 8080);
 
